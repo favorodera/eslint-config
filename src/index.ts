e31653372dd1143ba18589e794ba66693c89f206
Favour Emeka <favorodera@gmail.com>
2026-06-07 09:13:49 +0100

    feat(index): export tailwind config and presets
    - Export tailwind config and options
    - Export config presets
    - Export getModuleDefault utility

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ export { ignores, type IgnoresPatterns } from './configs/ignores'
 export { vue, type VueConfigOptions } from './configs/vue'
 export { typescript, type TypescriptConfigOptions } from './configs/typescript'
 export { stylistic, type StylisticConfigOptions } from './configs/stylistic'
+export { tailwind, type TailwindConfigOptions } from './configs/tailwind'
+
+// presets -- pre-configured sets of options
+export { allConfigOptionsFalse, allConfigOptionsTrue } from './config-presets'
+
+// utils - useful for consumers building on top
+export { getModuleDefault, renameRules } from './utils'
 
 // globs — useful when extending configs
 export { vueGlob, tsGlob, ignoresGlob } from './globs'
@@ -13,6 +20,3 @@ export { vueGlob, tsGlob, ignoresGlob } from './globs'
 // types
 export type { TypedFlatConfigItem, SharedOptions, Rules } from './types/utils'
 export type { RuleOptions, ConfigNames } from './types/rules'
-
-// utils — useful for consumers building on top
-export { renameRules } from './utils'
